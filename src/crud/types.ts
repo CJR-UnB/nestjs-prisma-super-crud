4d607eae4ae4727617a7b9c435fd79ca27619a18
25edson25 <edsondesouzasales@gmail.com>
2022-12-30 00:04:53 -0300

    feat/Adiciona tipo para o retorno dos métodos

--- src/crud/types.ts
+++ src/crud/types.ts
@@ -2,18 +2,15 @@ export interface BaseEntity {
     id: number;
 }
 
+// Falta testes para Entidades com relações
 type Option<Entity, CreateDto, UpdateDto> = {
     data: CreateDto | UpdateDto;
     where: Partial<Record<keyof Entity, any>>;
     select: Partial<Record<keyof Entity, any>>;
     orderBy: Partial<Record<keyof Entity, "asc" | "desc">>[];
     include: Partial<Record<keyof Entity, any>>;
-    // skip: number;
-    // take: number;
 };
 
-// Falta complementar distribuir corretamente entre os métodos as suas opções
-// Isso deve ser feito em cada método do Crud. Cortar as opções inválidas para o método
 export type DefaultOption<Entity, CreateDto, UpdateDto> = Partial<
     Option<Entity, CreateDto, UpdateDto>
 >;
@@ -51,19 +48,19 @@ type ModelDeleteOption<Entity, CreateDto, UpdateDto> = Pick<
 export interface Model<Entity extends BaseEntity, CreateDto, UpdateDto> {
     create(
         options: ModelCreateOption<Entity, CreateDto, UpdateDto>
-    ): Promise<any>;
+    ): Promise<ModelCreateOption<Entity, CreateDto, UpdateDto>["select"]>;
     findMany(
         options: ModelFindManyOption<Entity, CreateDto, UpdateDto>
-    ): Promise<any>;
+    ): Promise<ModelFindManyOption<Entity, CreateDto, UpdateDto>["select"][]>;
     findUnique(
         options: ModelFindUniqueOption<Entity, CreateDto, UpdateDto>
-    ): Promise<any>;
+    ): Promise<ModelFindUniqueOption<Entity, CreateDto, UpdateDto>['select']>;
     update(
         options: ModelUpdateOption<Entity, CreateDto, UpdateDto>
-    ): Promise<any>;
+    ): Promise<ModelUpdateOption<Entity, CreateDto, UpdateDto>['select']>;
     delete(
         options: ModelDeleteOption<Entity, CreateDto, UpdateDto>
-    ): Promise<any>;
+    ): Promise<ModelDeleteOption<Entity, CreateDto, UpdateDto>['select']>;
 }
 
 //**************** Tipos da Opções Personalizadas ****************
